76fe7616b5e3ab0955a85c5639dda02f8a8b3087
Miguel Prada <mapra99@gmail.com>
2020-11-23 00:49:17 -0500

    added query param to search by id or name

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -4,7 +4,9 @@ class BreedsController < ApplicationController
     render status: :bad_request and return unless query_term.present?
 
     images_limit = params[:include_images]&.to_i || 1
-    search = BreedServices::SearchService.new(query_term: query_term, include_images: images_limit)
+    search_by = params[:by]&.to_sym || :breed_name
+
+    search = BreedServices::SearchService.new(query_term: query_term, include_images: images_limit, search_by: search_by)
     search.perform
     search.save_search
     render json: search.results
