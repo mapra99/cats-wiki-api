cbb7e71f4475b9a6475e956cf10fe942931d3754
Miguel Prada <mapra99@gmail.com>
2020-11-27 18:41:36 -0500

    redis setup

--- app/models/breed_search.rb
+++ app/models/breed_search.rb
@@ -2,10 +2,12 @@ class BreedSearch < ApplicationRecord
   validates :query_term, presence: true
 
   def self.top_searches
-    where(succeed: true)
-      .group(:breed_id)
-      .count
-      .sort_by { |_breed, count| -count }
-      .map { |entry| { breed_id: entry[0], count: entry[1] } }
+    Rails.cache.fetch('breeds/top-searches', expires_in: 1.day) do
+      where(succeed: true)
+        .group(:breed_id)
+        .count
+        .sort_by { |_breed, count| -count }
+        .map { |entry| { breed_id: entry[0], count: entry[1] } }
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,11 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV['REDISCLOUD_URL'],
+                                             connect_timeout:    30,  # Defaults to 20 seconds
+                                             read_timeout:       0.2, # Defaults to 1 second
+                                             write_timeout:      0.2, # Defaults to 1 second
+                                             reconnect_attempts: 1 }   # Defaults to 0 }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
